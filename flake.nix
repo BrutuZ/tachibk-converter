0d4fa78f3678802dbe2e111126d675332ce7061c
BrutuZ <BrutuZ@users.noreply.github.com>
2025-10-26 22:29:51 -0300

    Update requirements

--- flake.nix
+++ flake.nix
@@ -3,9 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
-    utils = {
-      url = "github:numtide/flake-utils";
-    };
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
